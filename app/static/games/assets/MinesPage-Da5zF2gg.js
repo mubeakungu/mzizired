eca6bdf8ec6e4c00f2950f49f05100277a921f8d
moffatmubea <48717239+mubeakungu@users.noreply.github.com>
2026-08-25 08:34:17 +0300

    Add files via upload

---
+++ app/static/games/assets/MinesPage-Da5zF2gg.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-BlSC-vCk.js";function o(){return e.useEffect(()=>{window.location.href="/casino/play/mines"},[]),null}export{o as default};
---
+++ app/static/games/assets/PlinkoPage-DqcSNaMe.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-BlSC-vCk.js";function t(){return o.useEffect(()=>{window.location.href="/plinko-mzizi/"},[]),null}export{t as default};
